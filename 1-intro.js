e0a6d86bfef496bf279d5ce9d9d0f52a95f8eb9c
enrique-dominguez <enriquejosedominguez@gmail.com>
2024-06-07 13:58:51 -0400

    explored the global variables of node

---
+++ 1-intro.js
@@ -0,0 +1,12 @@
+const amount = 9
+
+if (amount < 10) {
+    console.log('small number');
+}
+else {
+    console.log('large number');
+}
+
+console.log(`hey it's my first node app!!!`)
+
+// Run this app by writing in the terminal "node app.js" , in 00:19:00 of the yt video
--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const amount = 9
+// GLOBALS - NO WINDOW !!!!
 
-if (amount < 10) {
-    console.log('small number');
-}
-else {
-    console.log('large number');
-}
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - infro about current module (file)
+// process - info about env where the program is being executed
 
-console.log(`hey it's my first node app!!!`)
+// console.log(__dirname)
+// console.log(__filename)
+setInterval(
+    ()=>{
+        console.log('hello world')
+    },1000) // this logs 'hello world" every second
+
+
+    // to end we press crtl+C
