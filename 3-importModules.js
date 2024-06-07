1a4628432c4dc9369eb859435ddd2d74d3ac1639
enrique-dominguez <enriquejosedominguez@gmail.com>
2024-06-07 16:59:10 -0400

    learning about built in modules and destructuring

---
+++ 3-importModules.js
@@ -0,0 +1,32 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimun)
+
+// The below vars were exported to the file '4-exportData'
+        // const john = 'john'
+        // const peter = 'peter'
+
+// const names = require('./4-exportData') // importing the name vars
+//console.log(names) // we received an object with two keys, the names
+
+
+const {john , peter} = require('./4-exportData') // object destructuring, shorter than using names.john or names.peter
+
+
+
+// The below function was placed in the file '5-exportFunc'
+        // const sayHi = (name) => {
+        //     console.log(`Hello there ${name}`)
+        // }
+
+
+const sayHi = require('./5-exportFunc') // importing the name vars
+
+const data = require('./6-altExports')
+
+// console.log(data)
+
+// sayHi('susan')
+// sayHi(john)
+// sayHi(peter)
+
+require('./7-nonExportedCalledFn') // this will call a function inside that module
--- app.js
+++ app.js
@@ -1,31 +1,19 @@
-// CommonJS, every file is module (by default)
-// Modules - Encapsulated Code (only share minimun)
+// Built-in modules
+// We can check all the built-in modules in node.org, specify the version to get accurate info.
 
-// The below vars were exported to the file '4-exportData'
-        // const john = 'john'
-        // const peter = 'peter'
+// os   : Operating system, provides, useful properties and methods to interact with the operating system and server
 
-// const names = require('./4-exportData') // importing the name vars
-//console.log(names) // we received an object with two keys, the names
+const os = require('os')
+    /* os is a built-in module, we don't have to install it.
+    We don't have to write a path, just 'os' and not './os*/
 
+const obj = {
+    a:1,
+    b:()=>console.log("hello"),
+}
 
-const {john , peter} = require('./4-exportData') // object destructuring, shorter than using names.john or names.peter
-
-
-
-// The below function was placed in the file '5-exportFunc'
-        // const sayHi = (name) => {
-        //     console.log(`Hello there ${name}`)
-        // }
-
-
-const sayHi = require('./5-exportFunc') // importing the name vars
-
-const data = require('./6-altExports')
-
-console.log(data)
-
-sayHi('susan')
-sayHi(john)
-sayHi(peter)
+obj.b() //59.22
+// path :
+// fs   : File system
+// HTTP :
 
