05798df8ec049ee4df1aaf8c245d722fcb92a830
enrique-dominguez <enriquejosedominguez@gmail.com>
2024-06-11 16:35:07 -0400

    first commit with the package.json 02:05:54

--- app.js
+++ app.js
@@ -1,57 +1,32 @@
-// http built-in module
+// npm = node package manager
 
-const http = require('http')
+// npm - global command, comes with node
+// npm --version OR --v
 
-const server = http.createServer( // receives a callback function as a parameter
-    (
-        req, // request
-        res //  response
-     ) => 
-        //{
-    //     // res.write('Welcome to our home page') // we can place this text inside the res.end()
-    //     // res.end()
+// local dependency - use it only in htis particular project (used more)
+// npm i <packageName>
 
-    //     // console.log(req) // the request object is massive
-
-    //     if (req.url === '/'){
-    //         res.end('Welcome to our home page')
-    //     }
-    //     if (req.url === '/about'){
-    //         res.end('About page')
-    //     }
-
-    //     // In the case of writing an unespecified Url
-    //     res.end(`
-    //         <h1>Opps!</h1>
-    //         <p>We can't seem to find the page you are looking for</p>
-    //         <a href="/">back home</a>
-    //     `)
-    // })
-
-    {
-        if (req.url === '/') { // better code, in the previous one we called res.end() twice and it threw an error because the respones was already ended, cant close twice
-            res.end('Welcome to our home page');
-        } else if (req.url === '/about') {
-            res.end('About page');
-        } else {
-            res.end(`
-                <h1>Oops!</h1>
-                <p>We can't seem to find the page you are looking for</p>
-                <a href="/">back home</a>
-            `);
-        }
-    });
+// global dependency - use it in any project
+// npm install - g <packageName>
+// sudo npm install - g <packageName> (mac)
 
 
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default) (faster)
 
 
-server.listen(5000, () => {
-    console.log('Server is listening on port 5000...');
-});
- // receives the port number
-// when running this code, go to the browser and check
-// localhost:5000
+// commands ran
+/*
+    npm init -y
+    npm i lodash
+    npm i bootstrap
+*/
 
-// to exit just prest Ctrl+C
+const _ = require('lodash') // lodash is very useful for data manipulation
 
-// this is our first website with node
+const items = [1 , [2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems) // [ 1, 2, 3, 4 ]
+
